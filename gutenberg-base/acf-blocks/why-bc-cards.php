0523088109612b5788b1fa801da5c312ffd24697
Daniel Todd <hello@proactivecode.com>
2023-02-23 12:59:57 +0000

    dev/ 991-2

--- gutenberg-base/acf-blocks/why-bc-cards.php
+++ gutenberg-base/acf-blocks/why-bc-cards.php
@@ -48,9 +48,11 @@
                     <div class="why-bc-cards__card <?php if($card['stand_out']) { echo 'why-bc-cards__card--stand-out'; } ?>">
                         <?php if(!$card['stand_out']) { ?>
                             <div class="why-bc-cards__image">
-                                <a href="<?php echo $card['link']['url']; ?>"  target="<?php echo $card['link']['target']; ?>">
-                                    <img src="<?php echo $card['image']['url']; ?>" alt="<?php echo $card['image']['alt']; ?>">
-                                </a>
+                                <?php if($card['link']['url']) { ?> 
+                                    <a href="<?php echo $card['link']['url']; ?>"  target="<?php echo $card['link']['target']; ?>">
+                                        <img src="<?php echo $card['image']['url']; ?>" alt="<?php echo $card['image']['alt']; ?>">
+                                    </a>
+                                <?php } ?>
                             </div>
                         <?php } ?>
                         <div class="why-bc-cards__content-wrapper">
